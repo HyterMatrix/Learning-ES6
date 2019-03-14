3d216e16b2843d1e26d117cdc25733c092733a82
HyterMatrix <admin@hyter.me>
2019-03-14 21:25:37 +0800

    ES6学习

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    title: 'ES6学习文档',
+    description: 'ES6学习文档',
+    themeConfig: {
+        nav: [
+            { text: '文档', link: '/' },
+        ],
+        sidebar: {
+            title: this.title,
+            collapsable: false,
+            children: [
+                '',
+            ]
+        },
+        lastUpdated: 'Last Updated',
+    },
+    base:'/Learning-ES6/',
+};
